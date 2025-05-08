f26a83363bac9902b4aea0a8a5daaa53d8eeabf7
Devorah135 <dschonko@student.touro.edu>
2025-05-08 17:20:06 -0400

    worked on adding sample data and some views

--- GrocerEase/core/views.py
+++ GrocerEase/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import ShoppingList
 
 # Create your views here.
+def shopping_list_view(request):
+    shopping_lists = ShoppingList.objects.all()
+    return render(request, 'shopping_list.html', {'shopping_lists': shopping_lists})
