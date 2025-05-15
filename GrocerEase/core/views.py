ef9852a339dfba872ac4e1024a11b608bc2453c3
Devorah135 <dschonko@student.touro.edu>
2025-05-15 15:42:32 -0400

    indentation

--- GrocerEase/core/views.py
+++ GrocerEase/core/views.py
@@ -1,35 +1,35 @@
 # core/views.py
-    from django.shortcuts import redirect, render
-    from .forms import AddItemForm
-    from .models import ListItem, ShoppingList
+from django.shortcuts import redirect, render
+from .forms import AddItemForm
+from .models import ListItem, ShoppingList
 
-    def shopping_list_view(request):
-        shopping_list, _ = ShoppingList.objects.get_or_create(user=request.user)
-        total_price = shopping_list.total_price()
+def shopping_list_view(request):
+    shopping_list, _ = ShoppingList.objects.get_or_create(user=request.user)
+    total_price = shopping_list.total_price()
 
-        if request.method == 'POST':
-            if 'delete_item' in request.POST:
-                item_id = request.POST.get('delete_item')
-                shopping_list.delete_item(item_id)
-                return redirect('shopping_list')
-            elif 'clear_list' in request.POST:
-                shopping_list.clear_list()
-                return redirect('shopping_list')
-            else:
-                form = AddItemForm(request.POST)
-                if form.is_valid():
-                    store_item = form.cleaned_data['item']
-                    quantity = form.cleaned_data['quantity']
-                    list_item, _ = ListItem.objects.get_or_create(item=store_item)
-                    list_item.item.quantity = quantity
-                    list_item.item.save()
-                    shopping_list.add_item(list_item)
-                    return redirect('shopping_list')
+    if request.method == 'POST':
+        if 'delete_item' in request.POST:
+            item_id = request.POST.get('delete_item')
+            shopping_list.delete_item(item_id)
+            return redirect('shopping_list')
+        elif 'clear_list' in request.POST:
+            shopping_list.clear_list()
+            return redirect('shopping_list')
         else:
-            form = AddItemForm()
+            form = AddItemForm(request.POST)
+            if form.is_valid():
+                store_item = form.cleaned_data['item']
+                quantity = form.cleaned_data['quantity']
+                list_item, _ = ListItem.objects.get_or_create(item=store_item)
+                list_item.item.quantity = quantity
+                list_item.item.save()
+                shopping_list.add_item(list_item)
+                return redirect('shopping_list')
+    else:
+        form = AddItemForm()
 
-        return render(request, 'shopping_list.html', {
-            'shopping_list': shopping_list,
-            'form': form,
-            'total_price': total_price
-        })
+    return render(request, 'shopping_list.html', {
+        'shopping_list': shopping_list,
+        'form': form,
+        'total_price': total_price
+    })
