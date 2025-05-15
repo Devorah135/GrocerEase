59ec8040c12c181d864279b6f0290b20b9896fee
Devorah135 <dschonko@student.touro.edu>
2025-05-15 17:12:00 -0400

    it works when u login :)

--- GrocerEase/core/views.py
+++ GrocerEase/core/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect, render
 from .forms import AddItemForm
 from .models import ListItem, ShoppingList
 
-@login_required
+#@login_required
 def shopping_list_view(request):
     shopping_list, _ = ShoppingList.objects.get_or_create(user=request.user)
     total_price = shopping_list.total_price()
