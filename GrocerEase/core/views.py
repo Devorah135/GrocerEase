b42652b273cd17dc42da435d6874a95fc5a70951
Ilana135 <irenov@student.touro.edu>
2025-05-15 15:38:20 -0400

    Added functionality to clear shopping list

--- GrocerEase/core/views.py
+++ GrocerEase/core/views.py
@@ -11,6 +11,29 @@ def shopping_list_view(request):
             item_id = request.POST.get('delete_item')
             shopping_list.delete_item(item_id)
             return redirect('shopping_list')
+        def shopping_list_view(request):
+            shopping_list, _ = ShoppingList.objects.get_or_create(user=request.user)
+            if request.method == 'POST':
+                if 'delete_item' in request.POST:
+                    item_id = request.POST.get('delete_item')
+                    shopping_list.delete_item(item_id)
+                    return redirect('shopping_list')
+                elif 'clear_list' in request.POST:
+                    shopping_list.clear_list()
+                    return redirect('shopping_list')
+                else:
+                    form = AddItemForm(request.POST)
+                    if form.is_valid():
+                        store_item = form.cleaned_data['item']
+                        quantity = form.cleaned_data['quantity']
+                        list_item, _ = ListItem.objects.get_or_create(item=store_item)
+                        list_item.item.quantity = quantity
+                        list_item.item.save()
+                        shopping_list.add_item(list_item)
+                        return redirect('shopping_list')
+            else:
+                form = AddItemForm()
+            return render(request, 'shopping_list.html', {'shopping_list': shopping_list, 'form': form})
         else:
             form = AddItemForm(request.POST)
             if form.is_valid():
