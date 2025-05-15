ff9e7c6fa3f010b563177a3b3fb1e38857356f4f
Devorah135 <dschonko@student.touro.edu>
2025-05-15 15:30:01 -0400

    added button to add item

--- GrocerEase/core/forms.py
+++ GrocerEase/core/forms.py
@@ -6,3 +6,7 @@ class ShoppingListForm(forms.ModelForm):
     class Meta:
         model = ShoppingList
         fields = ['items']  # Replace 'items' with the actual field(s) in your model
+
+class AddItemForm(forms.Form):
+    item = forms.ModelChoiceField(queryset=StoreItem.objects.all(), label="Item")
+    quantity = forms.IntegerField(min_value=1, label="Quantity")
--- GrocerEase/core/views.py
+++ GrocerEase/core/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from .models import ShoppingList
+# core/views.py
+from django.shortcuts import redirect, render
+from .forms import AddItemForm
+from .models import ListItem, ShoppingList
 
-# Create your views here.
+
 def shopping_list_view(request):
-    shopping_lists = ShoppingList.objects.all()
-    return render(request, 'shopping_list.html', {'shopping_lists': shopping_lists})
+    shopping_list, _ = ShoppingList.objects.get_or_create(user=request.user)
+    if request.method == 'POST':
+        form = AddItemForm(request.POST)
+        if form.is_valid():
+            store_item = form.cleaned_data['item']
+            quantity = form.cleaned_data['quantity']
+            list_item, _ = ListItem.objects.get_or_create(item=store_item)
+            list_item.item.quantity = quantity
+            list_item.item.save()
+            shopping_list.add_item(list_item)
+            return redirect('shopping_list')
+    else:
+        form = AddItemForm()
+    return render(request, 'shopping_list.html', {'shopping_list': shopping_list, 'form': form})
