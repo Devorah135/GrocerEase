4417089cc21640b461d6a1c083db15e926532fde
Devorah135 <dschonko@student.touro.edu>
2025-05-15 15:32:48 -0400

    added functionality to delete an item

--- GrocerEase/core/views.py
+++ GrocerEase/core/views.py
@@ -3,19 +3,24 @@ from django.shortcuts import redirect, render
 from .forms import AddItemForm
 from .models import ListItem, ShoppingList
 
-
+# core/views.py
 def shopping_list_view(request):
     shopping_list, _ = ShoppingList.objects.get_or_create(user=request.user)
     if request.method == 'POST':
-        form = AddItemForm(request.POST)
-        if form.is_valid():
-            store_item = form.cleaned_data['item']
-            quantity = form.cleaned_data['quantity']
-            list_item, _ = ListItem.objects.get_or_create(item=store_item)
-            list_item.item.quantity = quantity
-            list_item.item.save()
-            shopping_list.add_item(list_item)
+        if 'delete_item' in request.POST:
+            item_id = request.POST.get('delete_item')
+            shopping_list.delete_item(item_id)
             return redirect('shopping_list')
+        else:
+            form = AddItemForm(request.POST)
+            if form.is_valid():
+                store_item = form.cleaned_data['item']
+                quantity = form.cleaned_data['quantity']
+                list_item, _ = ListItem.objects.get_or_create(item=store_item)
+                list_item.item.quantity = quantity
+                list_item.item.save()
+                shopping_list.add_item(list_item)
+                return redirect('shopping_list')
     else:
         form = AddItemForm()
     return render(request, 'shopping_list.html', {'shopping_list': shopping_list, 'form': form})
