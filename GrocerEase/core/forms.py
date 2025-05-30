53a679afaf86a880da22cf8c30762023859f97d1
Ilana135 <irenov@student.touro.edu>
2025-05-30 13:48:17 -0400

    Cleaned up .pyc files and updated forms.py

--- GrocerEase/core/forms.py
+++ GrocerEase/core/forms.py
@@ -9,5 +9,6 @@ class ShoppingListForm(forms.ModelForm):
         fields = ['items']  # Replace 'items' with the actual field(s) in your model
 
 class AddItemForm(forms.Form):
-    item = forms.ModelChoiceField(queryset=StoreItem.objects.all(), label="Item")
-    quantity = forms.IntegerField(min_value=1, label="Quantity", initial=1)
+    item = forms.ModelChoiceField(queryset=StoreItem.objects.all(), required=False, label='Select from list')
+    manual_item_name = forms.CharField(max_length=100, required=False, label='Or enter item name')
+    quantity = forms.IntegerField(min_value=1, initial=1)
